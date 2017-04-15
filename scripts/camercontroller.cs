8b2a60e6ca2ae3d258fe2760048b6cce6a5596dc
nikhilaryan <nikhilaryann@gmail.com>
2017-04-16 03:23:00 +0530

    Add files via upload

---
+++ scripts/camercontroller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camercontroller : MonoBehaviour {
+    public GameObject sphere;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - sphere.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = sphere.transform.position + offset; 
+	}
+}
---
+++ scripts/playercontroller.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class playercontroller : MonoBehaviour {
+    public Text countText;
+    private Rigidbody rb;
+    private int count;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setcountText();
+
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float movehorizontal = Input.GetAxis("Horizontal");
+        float movevertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(movehorizontal, 0.0f, movevertical);
+        rb.AddForce(movement);
+	}
+    void OnTriggerEnter(Collider other)
+    {if (other.gameObject.CompareTag("pick")) ;
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            setcountText();
+        }
+    }
+    void setcountText() {
+        
+    }
+}
